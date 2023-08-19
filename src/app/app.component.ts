9eaa4878cd6ceb7a745cfe3636ae64751444cc15
Angie Vannesa Restrepo <avannesarestrepo@gmail.com>
2023-08-19 02:53:41 -0500

    property and event binding - no service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
+import { LoggingService } from './LoggingService.service';
+import { PersonasService } from './personas.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'listado-personas';
+  titulo = 'Listado de personas';
+  personas: Persona[] = [];
+
+  constructor(private loggingService: LoggingService, private personasService: PersonasService) {}
+
+  ngOnInit(): void{
+    this.personas = this.personasService.personas;
+  }
+
+
+  onPersonaAgregada(persona: Persona) {
+    this.loggingService.enviarMensajeAConsola("personas agregada al arreglo con nombre: " + persona.nombre);
+    this.personasService.personaAgregada(persona);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { PersonaComponent } from './persona/persona.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { LoggingService } from './LoggingService.service';
+import { PersonasService } from './personas.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent,
+    FormularioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
-  providers: [],
+  providers: [LoggingService, PersonasService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
